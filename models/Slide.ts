812c9bfc92f07acfc2578ea00bf5115c3e4ddfe4
Farid <84132083+fq-sys@users.noreply.github.com>
2025-10-21 20:26:40 +0400

    Create Slide model with schema definition

--- models/Slide.ts
+++ models/Slide.ts
@@ -1,1 +1,10 @@
+import mongoose, { Schema, model, models } from "mongoose"
 
+const SlideSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: "User" },
+  title: String,
+  audience: String,
+  slides: Array,
+}, { timestamps: true })
+
+export default models.Slide || model("Slide", SlideSchema)
